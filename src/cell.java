11cfa8a1ab78ddd13e768f4627a871efc843e3b3
aka334 <ayushkarki92@gmail.com>
2020-11-03 10:26:36 -0500

    comment for cell.java

--- src/cell.java
+++ src/cell.java
@@ -3,11 +3,13 @@ import java.awt.*;
 
 public class cell{
     int i, j, width;        //A position to define cell
-    boolean walls[];
-    boolean Visited;
-    boolean solveVisited, solveBacktracker;
-
+    boolean walls[];    //walls of a cell
+    boolean Visited;    //boolean to check if the cell has been visited or not
+    boolean solveVisited, solveBacktracker; //boolean value to store while solving the maze
+    //stroke for incresing line width
     Stroke stroke1 = new BasicStroke(3f);
+
+    //Constructor of cell function
     cell(int i ,int j, int width){
         this.i=  i;
         this.j = j;
@@ -17,13 +19,14 @@ public class cell{
         this.solveVisited =false;
         this.solveBacktracker = false;
     }
-
+    //Draw method for cell
     void draw(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(stroke1);
         g.setColor(Color.YELLOW);
         int x = this.i * width;
         int y = this.j * width;
+
         if(this.walls[0]){
             //top
             g2d.drawLine(x, y, x + this.width, y);
@@ -41,6 +44,7 @@ public class cell{
             g2d.drawLine(x, y + this.width, x, y);
         }
     }
+    //Highlight the cell with specified color
     void highlight(Graphics g, Color c, int wid){
         g.setColor(c);
         g.fillRect(this.i * width, this.j * width, wid, wid);
